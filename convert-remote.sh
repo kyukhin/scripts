3335679e48f89a83add33e139e6fd96f1304a7c4
Kirill Yukhin <kyukhin@tarantool.org>
2023-07-22 08:27:27 +0300

    ffmpegger: Implement remote conversion feature
    It is now possible to perform conversion on a remote
    host. No checks for scripts existance performed at all
    so far. Read in the scripts and fill env vars in env file.
    Make sure that all dirs and scripts exist on a remote side.
    Make sure that you access remote machine w/o SSH-agent-provided
    keys.

---
+++ convert-remote.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Add meta information to audio track
+# for i in *.avi ; do echo $i ; ffmpeg -i $i -map 0 -acodec copy -vcodec copy -metadata:s:a:0 language="rus" -metadata:s:a:1 language="eng" tmp.mkv && mv tmp.mkv ${i%.avi}.mkv ; done
+
+echo "Handling $1..."
+
+ARGS="${@:2}"
+
+echo "Args are: ${ARGS}"
+
+echo "ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} \"rm -rf ${VM_DIR_IN}/* ${VM_DIR_OUT}/*\""
+ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} "rm -rf ${VM_DIR_IN}/* ${VM_DIR_OUT}/*"
+if [ $? -ne 0 ]; then
+    echo "ERR: cleanup remote dirs. Exit."
+    exit 1
+fi
+
+echo "rsync -e \"ssh -i ${VM_KEY}\" --progress -av \"$1\" ${VM_USER}@${VM_HOST}:${VM_DIR_IN}"
+rsync -e "ssh -i ${VM_KEY}" --progress -av "$1" ${VM_USER}@${VM_HOST}:${VM_DIR_IN}
+if [ $? -ne 0 ]; then
+    echo "ERR: syncing to VM. Exit."
+    exit 1
+fi
+
+echo "ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} \"${VM_COMMAND} -v -f -d ${VM_DIR_IN} -o ${VM_DIR_OUT} -y ${ARGS}\""
+ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} "${VM_COMMAND} -v -f -d ${VM_DIR_IN} -o ${VM_DIR_OUT} -y ${ARGS}"
+if [ $? -ne 0 ]; then
+    echo "ERR: performing conversion. Exit."
+    # TODO: cleanup remote
+    exit 1
+fi
+
+echo "rsync -e \"ssh -i ${VM_KEY}\" --progress -av ${VM_USER}@${VM_HOST}:${VM_DIR_OUT} \"$1\""
+rsync -e "ssh -i ${VM_KEY}" --progress -av ${VM_USER}@${VM_HOST}:${VM_DIR_OUT} "$1"
+if [ $? -ne 0 ]; then
+    echo "ERR: syncing from VM. Exit."
+    exit 1
+fi
+
+echo "ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} \"rm -rf ${VM_DIR_IN}/* ${VM_DIR_OUT}/*\""
+ssh -i ${VM_KEY} ${VM_USER}@${VM_HOST} "rm -rf ${VM_DIR_IN}/* ${VM_DIR_OUT}/*"
+if [ $? -ne 0 ]; then
+    echo "ERR: cleanup remote dirs. Exit."
+    exit 1
+fi
---
+++ transmission-invoke-convert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+LOG="/tmp/transmission-invoke-convert.log"
+
+echo "Invoked new convert" >> $LOG
+echo -n "On " >> $LOG
+date >> $LOG
+
+echo "Location is $TR_TORRENT_DIR" >> $LOG
+echo "Torrent name is $TR_TORRENT_NAME" >> $LOG
+
+RPATH="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+echo "Path is: ${RPATH}" >> $LOG
+
+source ${HOME}/env
+
+echo "VM host: $VM_HOST" >> $LOG
+echo "VM user: $VM_USER" >> $LOG
+echo "VM key: $VM_KEY" >> $LOG
+echo "VM command: $VM_COMMAND" >> $LOG
+echo "VM dir in: $VM_DIR_IN" >> $LOG
+echo "VM dir out: $VM_DIR_OUT" >> $LOG
+
+if [[ ${RPATH} == *"0unsort/0convert"* ]]; then
+    echo "Will do conversion." >> $LOG
+    source ${HOME}/convert-remote.sh "${RPATH}" >> $LOG 2>&1
+else
+    echo "Will skip conversion." >> $LOG
+fi
+
+date >> $LOG
+echo "Done." >> $LOG
